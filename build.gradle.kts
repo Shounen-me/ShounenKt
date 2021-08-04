9f943b98ca38467238b19d19b0692f557ef0321e
Manuel Kour <manuelkour@gmail.com>
2021-08-04 11:16:33 +0200

    Subtle changes for Jitpack

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,25 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
     implementation("org.json:json:20090211")
-    implementation ("com.github.manuelkour:Shounen4J:master-SNAPSHOT")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "com.github.manuelkour"
+            artifactId = "Shounen4J"
+            version = "0.1"
+
+            from(components["java"])
+        }
+    }
+}
+
+tasks.jar {
+    manifest {
+        attributes(mapOf("Implementation-Title" to project.name,
+            "Implementation-Version" to project.version))
+    }
 }
 
 tasks.getByName<Test>("test") {
