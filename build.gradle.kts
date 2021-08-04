8f3b3ebffc6b24428201c5d3e1670025f7d05900
Manuel Kour <manuelkour@gmail.com>
2021-08-04 10:36:42 +0200

    Jitpack

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,21 @@
 plugins {
     java
+    `maven-publish`
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+group = "com.github.manuelkour"
+version = "0.1-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven { setUrl("https://jitpack.io") }
 }
 
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
     implementation("org.json:json:20090211")
+    implementation ("com.github.manuelkour:Shounen4J:master-SNAPSHOT")
 }
 
 tasks.getByName<Test>("test") {
